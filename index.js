5f2a53bae8f2ecfde012e20197b6a7f155fbc84b
Leily <leilymrz@gmail.com>
2021-05-27 17:48:58 -0700

    shop app created during bootcamp class - Colt Steele

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/shopApp', { userNewUrlParser: true })
+    .then(() => {
+        console.log('Mongo CONNECTION OPEN!')
+    })
+    .catch(err => {
+        console.log('Mongo OHH no error!')
+        console.log(err)
+    })
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/dogs', (req, res) => {
+    res.send('WOOF')
+})
+
+
+
+
+app.listen(3000, () => {
+    console.log('APP IS LISTENING ON PORT 3000')
+})
+
